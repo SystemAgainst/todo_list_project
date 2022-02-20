da7fe27ad6c2e328bf4836261931d0632ed6dde5
Tigran D <tigr.sa2021@gmail.com>
2022-02-20 13:40:02 +0300

    Added fitches about:
    * Form registration (from django solution)
    * Creation of user (and superuser for testing)
    * Error handling of passwords mismathe)
    * Error handling of username reuse

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signup_user(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signup_user.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            return render(request, 'todo/signup_user.html', {'form': UserCreationForm(), 'error': 'Passwords did\t match'})
